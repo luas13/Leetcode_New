56817cb254c15e643516f3893fcf407ec6a890ae
Abhishek Kumar <abhipmc@gmail.com>
2016-12-27 02:46:34 -0700

    Update Paint House.cpp

--- src/Paint House/Paint House.cpp
+++ src/Paint House/Paint House.cpp
@@ -1,7 +1,11 @@
 /*
-There are a row of n houses, each house can be painted with one of the three colors: red, blue or green. The cost of painting each house with a certain color is different. You have to paint all the houses such that no two adjacent houses have the same color.
+There are a row of n houses, each house can be painted with one of the three colors: red, blue or green. 
+The cost of painting each house with a certain color is different. You have to paint all the houses such that no two adjacent houses
+have the same color.
 
-The cost of painting each house with a certain color is represented by a n x 3 cost matrix. For example, costs[0][0] is the cost of painting house 0 with color red; costs[1][2] is the cost of painting house 1 with color green, and so on... Find the minimum cost to paint all houses.
+The cost of painting each house with a certain color is represented by a n x 3 cost matrix. For example, costs[0][0] is the cost
+of painting house 0 with color red; costs[1][2] is the cost of painting house 1 with color green, and so on... Find the minimum
+cost to paint all houses.
 
 Note
 All costs are positive integers.
@@ -10,7 +14,7 @@ All costs are positive integers.
 
 //Since its locked question. It hasn't passed OJ.
 
-//Recursive way of thinking
+//My Recursive way of thinking
 int fin(int cost[][], int index, int r, int v)
 {
 	if (r == cost.size())
@@ -19,15 +23,15 @@ int fin(int cost[][], int index, int r, int v)
 	else
 	{
 		return ( v + min( fin(cost, index, r+1, cost[r][(index - 1)%3]),
-						  fin(cost, index, r+1, cost[r][(index + 1)%3]) )
+				  fin(cost, index, r+1, cost[r][(index + 1)%3]) )
 	}
 }
 
 int fun(int cost[][])
 {
-	return min3( fin(cost, 0, 1, cost[0][0]),
-				 fin(cost, 1, 1, cost[0][1]),
-				 fin(cost, 2, 1, cost[0][2]) );
+    return min3( fin(cost, 0, 1, cost[0][0]),
+		 fin(cost, 1, 1, cost[0][1]),
+		 fin(cost, 2, 1, cost[0][2]) );
 }
 
 
@@ -35,26 +39,26 @@ int fun(int cost[][])
 class Solution {
 public:
     int minCost(vector<vector<int>>& costs) {
-		int R= costs.size();
-		int C= costs[0].size();
-		if (!R)
-			return 0;
-		if (R==1)
-			return min(cost[0][0], min(cost[0][1], cost[0][2]));
-			
-		vector<vector<int>> dp(R, vector<int>(C,0))
-		for (int i= R-1; i>=0; i--)
+	int R= costs.size();
+	int C= costs[0].size();
+	if (!R)
+	    return 0;
+	if (R==1)
+	    return min(cost[0][0], min(cost[0][1], cost[0][2]));
+		
+	vector<vector<int>> dp(R, vector<int>(C,0))
+	for (int i= R-1; i>=0; i--)
+	{
+            for (int j=C-1; j>=0; j--)
+	    {
+		if (i == R-1)
+		dp[i][j] = costs[i][j];
+		else
 		{
-			for (int j=C-1; j>=0; j--)
-			{
-				if (i == R-1)
-					dp[i][j] = costs[i][j];
-				else
-				{
-					dp[i][j] = cost[i][j] + min( dp[i+1][(j-1)%3], dp[i+1][(j+1)%3] );
-				}
-			}
+		    dp[i][j] = cost[i][j] + min( dp[i+1][(j-1)%3], dp[i+1][(j+1)%3] );
 		}
-		return min(dp[0][0], min(dp[0][1], dp[0][2]));
+	    }
 	}
+	return min(dp[0][0], min(dp[0][1], dp[0][2]));
+    }
 };

1fe11b5b1f21d5b8ae9b076f5f61e0ca3f42402d
Abhishek Kumar <abhipmc@gmail.com>
2017-01-24 23:20:28 -0700

    Update Next Permutation.cpp

--- src/Next Permutation/Next Permutation.cpp
+++ src/Next Permutation/Next Permutation.cpp
@@ -1,3 +1,28 @@
+/*
+Analysis
+
+The steps to solve this problem:
+1) scan from right to left, find the first element that is less than its previous one.
+
+4 5 6 3 2 1 
+  |
+  p
+2) scan from right to left, find the first element that is greater than p.
+
+4 5 6 3 2 1 
+    |
+    q
+3) swap p and q
+
+4 5 6 3 2 1 
+swap
+4 6 5 3 2 1 
+
+4) reverse elements [p+1, nums.length]
+
+4 6 1 2 3 5 
+*/
+
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {

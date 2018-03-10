75a1d618ae7ef87173474b5085f1839fd807cf57
Abhishek Kumar <akuma151@asu.edu>
2018-03-10 12:38:47 -0700

    Update Unique Paths.cpp

--- src/Unique-Paths/Unique Paths.cpp
+++ src/Unique-Paths/Unique Paths.cpp
@@ -10,6 +10,24 @@ paths.So we used only a 1D array and manipulated it every time.
 class Solution {
 public:
     int uniquePaths(int m, int n) {
+        if (!m || !n)
+            return 0;
+        
+        vector<int> dp(n, 1);
+        
+        for(int i=1; i<m; i++)
+            for(int j=1; j<n; j++)
+                dp[j] = dp[j] + dp[j-1];
+        
+        return dp[n-1];
+    }
+};
+
+OR,
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
         
         if( m==1 || n==1 ) 
             return 1;    

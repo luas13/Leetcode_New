71fcb75aebb9d5c12e75e83f3fce28935f603e13
Abhishek Kumar <akuma151@asu.edu>
2018-03-10 21:11:14 -0700

    Update Single Number II.cpp

--- src/Single Number II/Single Number II.cpp
+++ src/Single Number II/Single Number II.cpp
@@ -1,4 +1,32 @@
 /*
+!st check this solution:
+
+To solve this problem using only constant space, you have to rethink how the 
+numbers are being represented in computers – using bits. If you sum the ith bit 
+of all numbers and mod 3, it must be either 0 or 1 due to the constraint of this 
+problem where each number must appear either three times or once. This will be the 
+ith bit of that “single number”.
+
+A straightforward implementation is to use an array of size 32 to keep track of 
+the total count of ith bit.
+*/
+
+int singleNumber(int A[], int n) {
+    int count[32] = {0};
+    int result = 0;
+    for (int i = 0; i < 32; i++) {
+        for (int j = 0; j < n; j++) {
+            if ((A[j] >> i) & 1) {
+                count[i]++;
+            }
+        }
+        result |= ((count[i] % 3) << i);
+    }
+    return result;
+}
+
+_______________________________________________________________________________________________________
+/*
     ones as a bitmask to represent the ith bit had appeared once.
     twos as a bitmask to represent the ith bit had appeared twice.
     threes as a bitmask to represent the ith bit had appeared three times.

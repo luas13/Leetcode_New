3418a2a5fae8bfd50c26363a29509852f6c98fe8
Abhishek Kumar <akuma151@asu.edu>
2018-03-31 01:36:22 -0700

    Update Minimum Path Sum.cpp

--- src/Minimum-Path-Sum/Minimum Path Sum.cpp
+++ src/Minimum-Path-Sum/Minimum Path Sum.cpp
@@ -1,4 +1,38 @@
-//Very simple DP solution.The max optimization you can do is use O(m+n) space ,i.e. use two 1-D array,right and down
+/*
+The max optimization in space one can do is O(n) without changing the grid matrices
+*/
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int r = grid.size();
+        int c = grid[0].size();
+        
+        if (!r || !c)
+            return 0;
+        
+        vector<int> compr(c, 0);
+        compr[0] = grid[0][0];
+        for(int i=1; i<c; i++)
+            compr[i] = grid[0][i] + compr[i-1];
+        
+        for(int i=1; i<r; i++)
+        {
+            for(int j=0; j<c; j++)
+            {
+                if (j==0)
+                    compr[j] += grid[i][j];
+                else
+                    compr[j] = grid[i][j] + min(compr[j], compr[j-1]);
+            }
+        }
+        
+        return compr[c-1];
+    }
+};
+
+//---------------------------------------------------------------------------------------
+
+//Very simple DP solution.One optimization you can do is use O(m+n) space ,i.e. use two 1-D array,right and down
 //array.In this u just need to update down array once and the right array all through the loop
 
 class Solution {

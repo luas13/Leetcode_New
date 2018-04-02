a99ba164bc773aef4ac2650c4e467950ef3b1fe7
Abhishek Kumar <akuma151@asu.edu>
2018-04-01 20:53:23 -0700

    Update Repeated Substring Pattern.cpp

--- src/Repeated Substring Pattern/Repeated Substring Pattern.cpp
+++ src/Repeated Substring Pattern/Repeated Substring Pattern.cpp
@@ -1,3 +1,60 @@
+/*
+Using LPS array of KMP
+
+Imp. is return (lps[l-1] && l % (l-lps[l-1]) == 0);
+
+Here we cheack whether lps[l-1] is not 0 &
+l is divisible by l- lps[i], l-lps[i] is basically that string for which 
+there is no proper prefix & proper suffix
+
+*/
+
+class Solution {
+public:
+    vector<int> computeLPS(string s)
+    {
+        int l = s.length();
+        int i=1, j=0;
+        vector<int> lps(l, 0);
+        
+        while(i<l)
+        {
+            if (s[i] == s[j])
+            {
+                lps[i] = j+1;
+                i++;
+                j++;
+            }
+            else
+            {
+                if (j)
+                {
+                    j = lps[j-1];
+                }
+                else
+                {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+    
+    
+    bool repeatedSubstringPattern(string s) {
+        int l = s.length();
+        if (!l)
+            return true;
+        
+        vector<int> lps = computeLPS(s);
+        
+        return (lps[l-1] && l % (l-lps[l-1]) == 0);
+    }
+};
+
+
+//------------------------------------------------------------------------------
 class Solution {
 public:
     string leftshift(string str, int p)

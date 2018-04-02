f64cf978b4570d3c00e121783027d91e6409c62b
Abhishek Kumar <akuma151@asu.edu>
2018-04-01 22:42:21 -0700

    Update Ones and Zeroes.cpp

--- src/Ones and Zeroes/Ones and Zeroes.cpp
+++ src/Ones and Zeroes/Ones and Zeroes.cpp
@@ -2,8 +2,42 @@
 //requests, rather than listing all the problems, all likelihood is to use DP to solve, 
 //the top priority is to find Out recursive.
 
-//Similiar to knapsack problem
-//dp[i][j] stands for max number of str we can pick from strs with limitation of i "0"s and j "1"s
+/*
+Knapsack problem
+
+***Top Down Approach***
+int knapsack(int val[], int wt[], int n, int W)
+{
+   int dp[n+1][W+1];
+   memset(dp, 0, sizeof dp);
+   for (int i = 1; i <= n; i++)
+   {
+       for (w = 1; w <= W; w++)
+       {
+           dp[i][w] = dp[i-1][w];  //don't include the item
+           if (wt[i-1] <= w)
+                 dp[i][w] = max(dp[i][w], 
+                           val[i-1] + dp[i-1][w-wt[i-1]]);  
+       }
+   }
+   return dp[n][W];
+}
+
+
+***Bottom Up Approach***
+int knapsack(int val[], int wt[], int n, int W)
+{
+    int dp[W+1];
+    memset(dp, 0, sizeof(dp));
+    for(int i=0; i < n; i++) 
+        for(int j=W; j>=wt[i]; j--)
+            dp[j] = max(dp[j] , val[i] + dp[j-wt[i]]);
+    return dp[W];
+}
+
+This problem is similiar to knapsack problem Bottom Up Approach
+dp[i][j] stands for max number of str we can pick from strs with limitation of i "0"s and j "1"s
+*/
 
 class Solution {
 public:

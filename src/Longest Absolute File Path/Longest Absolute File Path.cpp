8a1a0950d72d4d9e2a893e22dfda6192edadf7b0
Abhishek Kumar <akuma151@asu.edu>
2018-04-10 14:07:57 -0700

    Update Longest Absolute File Path.cpp

--- src/Longest Absolute File Path/Longest Absolute File Path.cpp
+++ src/Longest Absolute File Path/Longest Absolute File Path.cpp
@@ -1,3 +1,63 @@
+// Soln 1
+class Solution {
+public:
+    int lengthLongestPath(string input) {
+        int l = input.length();
+        if (!l)
+            return l;
+        
+        vector<int> table(200);
+        int i=0, len=0, filemode=0;
+        
+        while(i<l && input[i] != '\n')
+        {
+            if (input[i] == '.')
+                filemode = 1;
+            i++;
+            len++;
+        }
+        cout<<len<<endl;
+        
+        // len+1 because of '/' term added
+        table[0] = len+1;
+        
+        int maxlen = INT_MIN;
+        if (filemode)
+            maxlen = len;
+        
+        while(i<l)
+        {
+            int level = 0;
+            filemode = 0;
+            i++;
+            
+            while(input[i] == '\t')
+            {
+                level++;
+                i++;
+            }
+            
+            len = 0;
+            while(i<l && input[i] != '\n')
+            {
+                if (input[i] == '.')
+                    filemode = 1;
+                i++;
+                len++;
+            }
+            
+            if (filemode)
+                maxlen = max(maxlen, table[level-1]+len);
+            
+            table[level] = table[level-1] + len+1;
+        }
+        
+        // if input is "aaaaaaaaaaaa", output should be 0
+        return maxlen==INT_MIN? 0: maxlen;
+    }
+};
+
+//---------------------
 // Without push_back operation. Max no of levels taken to be 200 ~ 0ms
 class Solution {
 public:

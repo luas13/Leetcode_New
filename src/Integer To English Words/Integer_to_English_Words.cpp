6a148254616829b6c0439a15ff7d211c68c71ff2
Abhishek Kumar <akuma151@asu.edu>
2018-05-23 23:38:13 -0700

    Update Integer_to_English_Words.cpp

--- src/Integer To English Words/Integer_to_English_Words.cpp
+++ src/Integer To English Words/Integer_to_English_Words.cpp
@@ -25,6 +25,33 @@ private:
         static const char* const below_20[];
         static const char* const below_100[];
         string helper(int n)
+        {   
+            if (n >= 1000000000) return helper(n/1000000000) + " Billion" + helper(n%1000000000);
+            else if (n >= 1000000) return helper(n/1000000) + " Million" + helper(n%1000000);
+            else if (n >= 1000) return helper(n/1000) + " Thousand" + helper(n%1000);
+            else if (n >= 100) return helper(n/100) + " Hundred" + helper(n%100);
+            else if (n >= 20) return string(" ") + below_100[n/10 - 2] + helper(n%10);
+            else if (n >= 1) return string(" ") + below_20[n -1];
+            else return "";
+        }
+    
+public:
+    string numberToWords(int num) {
+        if (num == 0)
+            return "Zero";
+        return helper(num).substr(1);
+    }
+};
+
+const char* const Solution::below_100[] = {"Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+const char* const Solution::below_20[] = {"One", "Two", "Three", "Four","Five","Six","Seven","Eight","Nine","Ten", "Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+
+// Without mod
+class Solution {
+private:
+        static const char* const below_20[];
+        static const char* const below_100[];
+        string helper(int n)
         {
             if (n >= 1000000000) return helper(n/1000000000) + " Billion" + helper(n - 1000000000 * (n/1000000000));
             else if (n >= 1000000) return helper(n/1000000) + " Million" + helper(n - 1000000 * (n/1000000));

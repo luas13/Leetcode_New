7a3d1b7b51c51cd7d1f7f613574e4315dad9bfd2
Abhishek Kumar <akuma151@asu.edu>
2019-04-23 16:30:36 -0700

    Update Permutations II.cpp

--- src/Permutations II/Permutations II.cpp
+++ src/Permutations II/Permutations II.cpp
@@ -1,3 +1,11 @@
+/*
+1. Maintain a vector of vector<int>, Initially this will contain the vector of num given
+2. Swap values at index 'pos' (pos starts with 0 til l-1) with "all the numbers present at
+pos+1 till l-1 one at a time". 
+3. Do this for every vector<int> present at any moment in the vector<vector<int>>.
+4. Make sure you first sort every vector<int> from 'pos' till end. 
+*/
+
 class Solution {
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
@@ -22,7 +30,7 @@ public:
                 // so purpose destroyed
                 sort(vv[i].begin() + pos, vv[i].end());
                 
-                //take each number to the first
+                //take each number after index pos to pos
                 for(int j= pos+1; j < vv[i].size(); j++){
                     vector<int> v= vv[i];
                     
@@ -43,3 +51,37 @@ public:
         return vv;
     }
 };
+
+
+/*
+Example:
+Input: [1,1,2,3,4,3]
+
+1 1 2 3 4 3
+after
+1 1 2 3 3 4
+2 1 1 3 3 4
+3 1 2 1 3 4
+4 1 2 3 3 1
+after
+1 1 2 3 3 4  // 1st old vector
+2 1 1 3 3 4  // 2nd old vector
+3 1 1 2 3 4  // 3rd old vector
+4 1 1 2 3 3  // 4th old vector
+
+1 2 1 3 3 4  // Swapped version of 1st old vector, 1st index value not disturbed, i.e. "1"
+1 3 2 1 3 4
+1 4 2 3 3 1
+
+2 3 1 1 3 4  // Swapped version of 2nd old vector, 1st index value not disturbed, i.e. "2"
+2 4 1 3 3 1
+
+3 2 1 1 3 4  // Swapped version of 3rd old vector, 1st index value not disturbed, i.e. "3"
+3 3 1 2 1 4
+3 4 1 2 3 1
+
+4 2 1 1 3 3  // Swapped version of 4th old vector, 1st index value not disturbed, i.e. "4"
+4 3 1 2 1 3 
+
+so on...
+*/

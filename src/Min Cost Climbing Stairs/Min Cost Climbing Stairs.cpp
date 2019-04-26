d5901b102de37fa1a9212932913f03e0d520e43b
Abhishek Kumar <akuma151@asu.edu>
2019-04-25 22:10:51 -0700

    Update Min Cost Climbing Stairs.cpp

--- src/Min Cost Climbing Stairs/Min Cost Climbing Stairs.cpp
+++ src/Min Cost Climbing Stairs/Min Cost Climbing Stairs.cpp
@@ -7,12 +7,9 @@ public:
         dp[0] = cost[0];
         dp[1] = cost[1];
         
-        int pprev = cost[0];
-        int prev = cost[1];
+        for(int i=2; i<l; i++)
+            dp[i] = min(dp[i-1], dp[i-2]) + cost[i];
         
-        for(int i=1; i<l; i++)
-            dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
-        
-        return min(dp[l-2], dp[l-1]);
+        return min(dp[l-1], dp[l-2]);
     }
 };

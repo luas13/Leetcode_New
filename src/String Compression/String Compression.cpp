d6e6f58823421df5dc0cd2939cb76443e8ed69a1
Abhishek Kumar <akuma151@asu.edu>
2019-04-27 16:04:07 -0700

    Update String Compression.cpp

--- src/String Compression/String Compression.cpp
+++ src/String Compression/String Compression.cpp
@@ -1,19 +1,31 @@
+/*
+i: new index after a repeated character is processed, 
+eg. aaaabb, after a is processed i is updated to 4 from 0
+
+t: index upto which the compressed string go
+j: to iterate the given string
+*/
 class Solution {
 public:
-    int compress(vector<char>& a) {
-        int i = 0, cnt = 0, t = 0;
-        for (int j = 0; j < a.size(); ++j) 
-            if (j+1 == a.size() || a[j+1] != a[j]) 
+    int compress(vector<char>& chars) {
+        int l = chars.size();
+        if (!l)
+            return 0;
+        
+        int i=0, t=0;
+        for(int j=0; j<l; j++)
+        {
+            if (j+1 == l || chars[j] != chars[j+1])
             {
-                int m = t;
-                a[t++] = a[j];
-                if (j > i) {
+                chars[t++] = chars[j];
+                if (j>i)
+                {
                     string s = to_string(j-i+1);
-                    for (char c:s) a[t++] = c;
+                    for(char c: s) chars[t++] = c;
                 }
-                i = j + 1;
-                cnt += t-m;
+                i = j+1;
             }
-        return cnt;
+        }
+        return t;
     }
 };

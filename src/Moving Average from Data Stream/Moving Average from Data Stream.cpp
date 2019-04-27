f577d715972bf51ff66cf52ba43edd8487893ae6
Abhishek Kumar <akuma151@asu.edu>
2019-04-27 16:28:49 -0700

    Update Moving Average from Data Stream.cpp

--- src/Moving Average from Data Stream/Moving Average from Data Stream.cpp
+++ src/Moving Average from Data Stream/Moving Average from Data Stream.cpp
@@ -2,13 +2,18 @@
 Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
 
 For example,
-MovingAverage m = new MovingAverage(3);
+MovingAverage m(3);
 m.next(1) = 1
 m.next(10) = (1 + 10) / 2
 m.next(3) = (1 + 10 + 3) / 3
 m.next(5) = (10 + 3 + 5) / 3
 
 */
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
 
 class MovingAverage{
 private:
@@ -19,19 +24,43 @@ private:
 public:
 	MovingAverage(int s)
 	{
-		this->size = s;
-		this->sum = 0;
+		size = s;
+		sum = 0;
 	}
 
+	void printq(queue<int> q)
+	{
+	    cout<<"Current queue is: ";
+	    while(!q.empty())
+	    {
+	        cout<<q.front()<<" ";
+	        q.pop();
+	    }
+	}
+	
 	double next(int v)
 	{
-		if (avgQueue.size() > size)
+		if (avgQueue.size() >= size)
 		{
 			sum -= avgQueue.front();
 			avgQueue.pop();
 		}
 		avgQueue.push(v);
 		sum += v;
+		
+		printq(avgQueue);
 		return sum/avgQueue.size();
 	}
 };
+
+int main()
+{
+    MovingAverage m(3);
+    
+    cout<<"\nMoving Avg: "<<m.next(1)<<endl;
+    cout<<"\nMoving Avg: "<<m.next(10)<<endl;
+    cout<<"\nMoving Avg: "<<m.next(4)<<endl;
+    cout<<"\nMoving Avg: "<<m.next(3)<<endl;
+    
+    return 0;
+}

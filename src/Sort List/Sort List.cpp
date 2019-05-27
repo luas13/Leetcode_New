9f23529f1a2465bc9cd29f63bc62e081e868fe22
Abhishek Kumar <akuma151@asu.edu>
2019-05-26 23:29:58 -0700

    Update Sort List.cpp

--- src/Sort List/Sort List.cpp
+++ src/Sort List/Sort List.cpp
@@ -19,18 +19,14 @@ public:
             if (l1->val < l2->val){
                 head->next = l1;
                 l1 = l1->next;
-            }
-            
+            }          
             else{
                 head->next = l2;
                 l2 = l2->next;
             }
-            
             head = head->next;
         }
-        
         head->next = l1? l1 : l2;
-        
         return dummy.next;
     }
     

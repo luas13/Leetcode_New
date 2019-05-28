01fc9cca279c223352df3a28f0708c7b7e495f05
Abhishek Kumar <akuma151@asu.edu>
2019-05-28 00:04:01 -0400

    Update min_stack.cpp

--- src/Min_Stack/min_stack.cpp
+++ src/Min_Stack/min_stack.cpp
@@ -30,9 +30,10 @@ Important Points:
 
 */
 
+/* runtime error if x = INT_MIN and you do 2*x
 class MinStack {
 public:
-    /** initialize your data structure here. */
+    // initialize your data structure here. 
     stack<long> s;
     long minEle;
     
@@ -87,12 +88,108 @@ public:
         return minEle;
     }
 };
+*/
+
+// changed to long(x)
+class MinStack {
+    long minE;
+    stack<long> st;
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+        
+    }
+    
+    void push(int x) {
+        if(st.empty())
+        {
+            st.push(x);
+            minE = x;
+        }
+        
+        else{
+            if (x < minE)
+            {
+                st.push(2*long(x) - minE);
+                minE = x;
+            }
+            else
+                st.push(x);
+        }
+    }
+    
+    void pop() {
+        if (st.empty())
+            return;
+        if (st.top() < minE)
+            minE = 2*long(minE) - st.top();
+        st.pop();
+    }
+    
+    int top() {
+        if(st.empty())
+            return -1;
+        if (st.top() < minE)
+            return minE;
+        else
+            return st.top();
+    }
+    
+    int getMin() {
+        return minE;
+    }
+};
 
 /**
  * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(x);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
  */
+
+
+// Soln: 2 Using 2 stacks
+
+class MinStack {
+    
+    stack<int> data;
+    stack<int> min;
+
+public:
+
+    void push(int x) {
+        
+        // If empty
+        if (min.empty()) {
+            data.push(x);
+            min.push(x);
+        }
+        
+        // Not empty
+        else {
+            data.push(x);
+            if (x <= min.top())
+                min.push(x);
+        }
+
+    }
+
+    void pop() {
+        
+        if (!min.empty()) {
+            if (data.top() == min.top())
+                min.pop();
+            data.pop();
+        }
+    }
+
+    int top() {
+        return data.top();
+    }
+
+    int getMin() {
+        return min.top();
+    }
+};
